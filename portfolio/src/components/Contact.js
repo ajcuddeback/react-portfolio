f7f7177a61a07897b499ac641bce8d196cf03b48
ajcudd <ajcuddeback@gmail.com>
2021-03-08 16:25:37 -0500

    added style to contact page

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+import {device} from '../devices';
 
 const Contact = () => {
     return (
-        <section className="contact" id="contact">
+        <StyledContact id="contact">
             <div className="title-flex">
                 <h2 className="main-titles">Contact Me</h2>
             </div>
@@ -19,8 +21,67 @@ const Contact = () => {
                     <button type="submit">Send</button>
                 </form>
             </div>
-        </section>
+        </StyledContact>
     )
 }
 
+const StyledContact = styled.section `
+    .contact-wrapper {
+        width: 100%;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    input[type="text"],
+    textarea {
+        width: 1000px;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    button[type="submit"] {
+        width: 1000px;
+        background-color: blue;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    @media ${device.laptop} {
+        input[type="text"],
+        textarea {
+            width: 700px;
+        }
+        button[type="submit"] {
+            width: 700px;
+        }
+    }
+    @media ${device.tablet} {
+        input[type="text"],
+        textarea {
+            width: 400px;
+        }
+        button[type="submit"] {
+            width: 400px;
+        }
+    }
+    @media ${device.mobileL} {
+        input[type="text"],
+        textarea {
+            width: 300px;
+        }
+        button[type="submit"] {
+            width: 300px;
+        }
+    }
+    
+`
 export default Contact;
